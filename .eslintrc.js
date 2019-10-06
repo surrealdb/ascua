05dfea9da59d4010c08d6c3a8e176fcbbf952e6e
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-10-06 15:03:21 +0100

    Upgrade to latest ember octane

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,12 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-jquery': 'error'
   },
   overrides: [
     // node files
     {
       files: [
+        '.ember-cli.js',
         '.eslintrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
@@ -31,8 +31,12 @@ module.exports = {
         'lib/*/index.js',
         'server/**/*.js'
       ],
+      excludedFiles: [
+        'app/**',
+      ],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
+        ecmaVersion: 2015
       },
       env: {
         browser: false,
@@ -45,7 +49,10 @@ module.exports = {
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
         'node/no-unpublished-require': 'off'
-      })
+      }),
+      extends: [
+        'plugin:node/recommended'
+      ],
     }
   ]
 };
