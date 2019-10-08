49ba706460c75b4aec0e0e4b35f2b27185fc083b
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-10-08 11:28:17 +0100

    Add droppable package

---
+++ packages/ember-droppable/addon/components/droppable.js
@@ -0,0 +1,113 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { computed } from '@ember/object';
+import { action } from '@ember/object';
+
+export default class extends Component {
+
+	@tracked waiting = false;
+
+	@tracked loading = false;
+
+	@tracked dropping = false;
+
+	@computed('args.width')
+	get width() {
+		return this.args.width || '100%';
+	}
+
+	@computed('args.height')
+	get height() {
+		return this.args.height || '100%';
+	}
+
+	@action didClick(event) {
+		event.target.querySelectorAll('input')[0].click();
+	}
+
+	@action didInput(event) {
+		this.into(event.target.files);
+	}
+
+	@action didDragOver(event) {
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	@action didDragEnter(event) {
+		this.dropping = true;
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	@action didDragLeave(event) {
+		this.dropping = false;
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	@action didDrop(event) {
+		this.dropping = false;
+		event.preventDefault();
+		event.stopPropagation();
+		if (event.dataTransfer) {
+			if (event.dataTransfer.files.length > 0) {
+				return this.into(event.dataTransfer.files);
+			}
+		}
+	}
+
+	into(files) {
+
+		let ms = [].concat(this.args.mimetypes).filter(Boolean);
+		let es = [].concat(this.args.extensions).filter(Boolean);
+
+		for (let i=0; i<files.length; i++) {
+
+			let f = files[i];
+
+			if (es.length && es.includes(f.ext) === false) {
+				return;
+			}
+
+			if (ms.length && ms.includes(f.type) === false) {
+				return;
+			}
+
+			let r = new FileReader();
+
+			if (this.args.onOpen) {
+				this.args.onOpen(f);
+			}
+
+			r.onload = e => {
+				this.waiting = true;
+				this.loading = false;
+				if (this.args.onLoad) {
+					this.args.onLoad(e, f);
+				}
+			}
+
+			r.onerror = e => {
+				this.waiting = true;
+				this.loading = false;
+				if (this.args.onError) {
+					this.args.onError(e, f);
+				}
+			}
+
+			r.onprogress = e => {
+				this.waiting = false;
+				this.loading = true;
+				if (this.args.onProgress) {
+					this.args.onProgress(e, f);
+				}
+			}
+
+			r.readAsBinaryString(f);
+
+		}
+
+	}
+
+}
