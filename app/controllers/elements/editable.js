4917ae17f9091535ecf6b941a9551bd0a591a8bc
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-10-14 00:50:04 +0100

    Add initial documentation

---
+++ app/controllers/elements/editable.js
@@ -0,0 +1,9 @@
+import Controller from '@ember/controller';
+
+export default class extends Controller {
+
+	text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent luctus sem tellus, at ullamcorper nulla hendrerit et. Etiam et urna non nisl rutrum molestie ut sed justo. Mauris in massa tellus. Praesent aliquet est in nisi laoreet venenatis. Sed posuere velit hendrerit massa auctor, in varius diam mollis. Praesent viverra lacus justo, blandit laoreet purus varius nec. Donec egestas tortor vel est suscipit faucibus.";
+
+	html = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent luctus sem tellus, at ullamcorper nulla hendrerit et. Etiam et urna non nisl rutrum molestie ut sed justo. Mauris in massa tellus. Praesent <i>aliquet est in nisi laoreet</i> venenatis. Sed posuere velit hendrerit massa auctor, in varius diam mollis. Praesent viverra lacus justo, <b>blandit laoreet</b> purus varius nec. Donec egestas tortor vel est suscipit faucibus.";
+
+}
--- app/router.js
+++ app/router.js
@@ -2,9 +2,76 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+	location = config.locationType;
+	rootURL = config.rootURL;
 }
 
 Router.map(function() {
+
+	this.route('config');
+	this.route('styling');
+
+	this.route('guides', function() {
+		this.route('pwas');
+		this.route('building');
+		this.route('electron');
+		this.route('fastboot');
+	});
+
+	this.route('services', function() {
+		this.route('clock');
+		this.route('electron');
+		this.route('internet');
+		this.route('location');
+		this.route('metrics');
+		this.route('storage');
+		this.route('update');
+		this.route('worker');
+	});
+
+	this.route('libraries', function() {
+		this.route('apexcharts');
+		this.route('chart');
+		this.route('codemirror');
+		this.route('mapbox-gl');
+		this.route('marked');
+		this.route('moment');
+		this.route('sheet');
+		this.route('stripe');
+	});
+
+	this.route('elements', function() {
+		this.route('codemirror');
+		this.route('contextmenu');
+		this.route('dropdown');
+		this.route('droppable');
+		this.route('editable');
+		this.route('iframe');
+		this.route('gravatar');
+		this.route('gridlist');
+		this.route('initials');
+		this.route('image');
+		this.route('input');
+		this.route('link');
+		this.route('modal');
+		this.route('select');
+		this.route('social');
+		this.route('label');
+		this.route('tooltip');
+		this.route('webview');
+	});
+
+	this.route('helpers', function() {
+		this.route('arrays');
+		this.route('dom');
+		this.route('events');
+		this.route('format');
+		this.route('keyboard');
+		this.route('logic');
+		this.route('maths');
+		this.route('promise');
+		this.route('routing');
+		this.route('strings');
+	});
+
 });
