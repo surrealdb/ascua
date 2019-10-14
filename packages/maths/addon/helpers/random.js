5909650e248a19d43be3ae475dbb2fbbcb58bdb6
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-10-14 12:25:19 +0100

    Fix maths random helper

--- packages/maths/addon/helpers/random.js
+++ packages/maths/addon/helpers/random.js
@@ -1,9 +1,8 @@
 import { helper } from '@ember/component/helper';
 
-export function random([min, max]) {
-	return (params) => {
-		return Math.random(params[0] || min, params[1] || max);
-	};
+export function random([min = 0, max = 0]) {
+	if (max < min) [min, max] = [max, min];
+	return Math.floor(Math.random() * max-min) + min;
 }
 
 export default helper(random);
