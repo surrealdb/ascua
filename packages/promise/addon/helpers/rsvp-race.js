24c187bd6c0dd6f0ab3fc052f412840f88fd27de
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-10-14 12:25:31 +0100

    Remove rsvp-race helper

--- packages/promise/addon/helpers/rsvp-race.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import RSVP from 'rsvp';
-
-export function rsvpRace(...promises) {
-	return RSVP.race(promises);
-}
-
-export default helper(revpRace);
--- packages/promise/app/helpers/rsvp-race.js
+++
@@ -1,1 +0,0 @@
-export { default } from '@ascua/promise/helpers/rsvp-race';
