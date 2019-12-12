847f00c01a9438e695942977b31835fcb097800f
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-12-12 10:52:55 +0000

    Update context package

--- packages/context/addon/index.js
+++ packages/context/addon/index.js
@@ -6,7 +6,7 @@ const noop = () => {};
 export default class Context {
 
 	static Background() {
-		return new Context();
+		return new Context(new Promise(() => {}));
 	}
 
 	static withCancel(parent) {
@@ -36,17 +36,17 @@ export default class Context {
 	withCancel() {
 		let cancel, cancelled = new Promise(r => cancel = r);
 		let context = new Context(cancelled);
-		this.cancelled.then(error => cancel(error || cancError), noop);
-		return { context, cancel };
+		this.cancelled.then(error => cancel(cancError));
+		return [ context, cancel ];
 	}
 
 	withTimeout(ms) {
-		let { context, cancel } = this.withCancel();
-		this.delay(ms).then(error => cancel(error || timeError), noop);
-		return { context, cancel };
+		let [ context, cancel ] = this.withCancel();
+		this.delay(ms).then(error => cancel(timeError));
+		return [ context, cancel ];
 	}
 
-	delay() {
+	delay(ms) {
 		return new Promise( (resolve, reject) => {
 			let timer = setTimeout(resolve, ms);
 			this.cancelled.then(error => {
