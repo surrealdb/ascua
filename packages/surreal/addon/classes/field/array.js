e941a0bd86af6337a327488823c3e0944aa64a02
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-01-23 12:54:56 +0000

    Ensure remote records are only fetched when requested

--- packages/surreal/addon/classes/field/array.js
+++ packages/surreal/addon/classes/field/array.js
@@ -11,80 +11,94 @@ import Field from '@ascua/surreal/field';
 import { assert } from '@ember/debug';
 
 export default function(type) {
-	return computed({
-		get(key) {
+	return (target, key, desc) => {
+		return {
+			configurable: true,
+			enumerable: true,
+			get() {
 
-			switch (type) {
-			case undefined:
-				return this.data[key] = this.data[key] || new Array(this, Any);
-			case 'string':
-				return this.data[key] = this.data[key] || new Array(this, String);
-			case 'number':
-				return this.data[key] = this.data[key] || new Array(this, Number);
-			case 'boolean':
-				return this.data[key] = this.data[key] || new Array(this, Boolean);
-			case 'datetime':
-				return this.data[key] = this.data[key] || new Array(this, Datetime);
-			default:
-
-				let model = this.store.lookup(type);
-
-				switch (true) {
-				case model && model.class.prototype instanceof Model:
-					return this.data[key] = this.data[key] || new Array(this, (v) => {
-						v = String(v);
-						return Record.create({
-							id: v, promise: this.store.select(type, v)
-						});
-					});
-				case model && model.class.prototype instanceof Field:
-					return this.data[key] = this.data[key] || new Array(this, (v) => {
-						let fields = Object.assign({}, v, { _parent: this });
-						return model.create(fields);
-					});
+				switch (type) {
+				case undefined:
+					return this.data[key] = this.data[key] || new Array(this, Any);
+				case 'string':
+					return this.data[key] = this.data[key] || new Array(this, String);
+				case 'number':
+					return this.data[key] = this.data[key] || new Array(this, Number);
+				case 'boolean':
+					return this.data[key] = this.data[key] || new Array(this, Boolean);
+				case 'datetime':
+					return this.data[key] = this.data[key] || new Array(this, Datetime);
 				default:
+
+					let value = this.data[key] || [];
+
+					let model = this.store.lookup(type);
+
+					if (model && model.class.prototype instanceof Field) {
+						return this.data[key] = this.data[key] || new Array(this, (v) => {
+							let fields = Object.assign({}, v, { _parent: this });
+							return model.create(fields);
+						}, ...value);
+					}
+
+					if (model && model.class.prototype instanceof Model) {
+						return this.data[key] = new Array(this, (v) => {
+							switch (true) {
+							case v instanceof Record:
+								return v;
+							default:
+								return Record.create({
+									id: v, promise: this.store.select(type, v)
+								});
+							}
+						}, ...value);
+					}
+
 					assert('An embedded object must be of type Model or Field');
+
+				}
+
+			},
+			set(value=[]) {
+
+				switch (type) {
+				case undefined:
+					return this.data[key] = new Array(this, Any, ...value);
+				case 'string':
+					return this.data[key] = new Array(this, String, ...value);
+				case 'number':
+					return this.data[key] = new Array(this, Number, ...value);
+				case 'boolean':
+					return this.data[key] = new Array(this, Boolean, ...value);
+				case 'datetime':
+					return this.data[key] = new Array(this, Datetime, ...value);
+				default:
+
+					let model = this.store.lookup(type);
+
+					if (model && model.class.prototype instanceof Field) {
+						return this.data[key] = new Array(this, (v) => {
+							let fields = Object.assign({}, v, { _parent: this });
+							return model.create(fields);
+						}, ...value);
+					}
+
+					if (model && model.class.prototype instanceof Model) {
+						return this.data[key] = new Array(this, (v) => {
+							switch (true) {
+							case v instanceof Record:
+								return v;
+							default:
+								return String(v);
+							}
+						}, ...value);
+					}
+
+					assert('An embedded object must be of type Model or Field');
+
 				}
 
 			}
-
-		},
-		set(key, value=[]) {
-
-			switch (type) {
-			case undefined:
-				return this.data[key] = new Array(this, Any, ...value);
-			case 'string':
-				return this.data[key] = new Array(this, String, ...value);
-			case 'number':
-				return this.data[key] = new Array(this, Number, ...value);
-			case 'boolean':
-				return this.data[key] = new Array(this, Boolean, ...value);
-			case 'datetime':
-				return this.data[key] = new Array(this, Datetime, ...value);
-			default:
-
-				let model = this.store.lookup(type);
-
-				switch (true) {
-				case model && model.class.prototype instanceof Model:
-					return this.data[key] = new Array(this, (v) => {
-						v = String(v);
-						return Record.create({
-							id: v, promise: this.store.select(type, v)
-						});
-					}, ...value);
-				case model && model.class.prototype instanceof Field:
-					return this.data[key] = new Array(this, (v) => {
-						let fields = Object.assign({}, v, { _parent: this });
-						return model.create(fields);
-					}, ...value);
-				default:
-					assert('An embedded object must be of type Model or Field');
-				}
-
-			}
-
 		}
-	});
+	}
 }
--- packages/surreal/addon/classes/field/record.js
+++ packages/surreal/addon/classes/field/record.js
@@ -3,25 +3,35 @@ import Record from '../types/record';
 import Model from '@ascua/surreal/model';
 
 export default function(type) {
-	return computed({
-		get(key) {
-			return this.data[key];
-		},
-		set(key, value) {
+	return (target, key, desc) => {
+		return {
+			configurable: true,
+			enumerable: true,
+			get() {
 
-			if (value instanceof Model) {
-				value = String(value);
+				let value = this.data[key];
+
+				switch (true) {
+				case typeof value === "undefined":
+					return this.data[key];
+				case value instanceof Record:
+					return this.data[key];
+				default:
+					return this.data[key] = Record.create({
+						id: value, promise: this.store.select(type, value),
+					});
+				}
+
+			},
+			set(value) {
+
+				if (value instanceof Model) {
+					value = String(value);
+				}
+
+				this.data[key] = value;
+
 			}
-
-			switch (value) {
-			case undefined:
-				return this.data[key] = undefined;
-			default:
-				return this.data[key] = Record.create({
-					id: value, promise: this.store.select(type, value),
-				});
-			}
-
 		}
-	});
+	}
 }
