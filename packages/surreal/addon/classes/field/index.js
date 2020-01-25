3d94493386bd515c337e2f650a3267893931efa6
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-01-25 19:06:32 +0000

    Ensure embedded fields have access to the store

--- packages/surreal/addon/classes/field/index.js
+++ packages/surreal/addon/classes/field/index.js
@@ -1,7 +1,9 @@
 import Core from '@ember/object';
-import { tracked } from '@glimmer/tracking';
+import { inject } from '@ember/service';
 
 export default class Field extends Core {
+
+	@inject store;
 
 	#data = {};
 
