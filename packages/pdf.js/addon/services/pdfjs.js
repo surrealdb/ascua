a623f9ed5d6bef94b472edd9c93bd78558a04fd2
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-02-01 17:47:41 +0000

    Ensure pdfjs path is correctly set

--- packages/pdf.js/addon/services/pdfjs.js
+++ packages/pdf.js/addon/services/pdfjs.js
@@ -1,23 +1,28 @@
 /* globals pdfjsLib */
 import Service from '@ember/service';
+import { inject } from '@ember/service';
 
 export default class extends Service {
 
+	@inject router;
+
 	async load() {
+
+		let base = `${this.router.rootURL}/assets`.replace(/\/\/+/g, '/');
 
 		if (this.loader === undefined) {
 			this.loader = new Promise( (resolve, reject) => {
 				let script = document.createElement('script');
 				script.onload = resolve;
 				script.onerror = reject;
-				script.src = '/assets/pdfjs.js';
+				script.src = `${base}/pdfjs.js`;
 				document.head.appendChild(script);
 			});
 		}
 
 		await this.loader;
 
-		pdfjsLib.GlobalWorkerOptions.workerSrc = '/assets/pdfjs-worker.js';
+		pdfjsLib.GlobalWorkerOptions.workerSrc = `${base}/pdfjs-worker.js`;
 
 		return pdfjsLib;
 
