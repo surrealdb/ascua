814a1794d19ec563714bd3259d8990173ffa68cc
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-02-09 00:12:46 +0000

    Improve autogrow textarea functionality

--- packages/input/addon/components/area.js
+++ packages/input/addon/components/area.js
@@ -11,15 +11,24 @@ export default class extends Component {
 
 	@action didCreate(element) {
 		this.element = element;
+		element.style.height = 'auto';
+		element.style.overflow = 'hidden';
+		element.style.height = `${element.scrollHeight}px`;
 	}
 
 	@action didChange(element) {
-		if (element !== document.activeElement) {
-			this.value = this.args.value;
-		}
+		this.value = this.args.value;
+		element.value = this.args.value;
+		element.style.height = 'auto';
+		element.style.overflow = 'hidden';
+		element.style.height = `${element.scrollHeight}px`;
 	}
 
 	@action didInput() {
+		this.element.value = this.element.value;
+		this.element.style.height = 'auto';
+		this.element.style.overflow = 'hidden';
+		this.element.style.height = `${this.element.scrollHeight}px`;
 		if (this.args.onChange) {
 			this.args.onChange(this.element.value);
 		}
@@ -29,14 +38,6 @@ export default class extends Component {
 		if (event.which !== 13) return true;
 		if (this.args.limit == false) return true;
 		return (this.element.value.split('\n').length < this.args.limit);
-	}
-
-	@action didPress() {
-		setTimeout( () => {
-			this.element.style.height = 'auto';
-			this.element.style.overflow = 'hidden';
-			this.element.style.height = `${this.element.scrollHeight}px`;
-		});
 	}
 
 }
