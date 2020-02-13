26c1a1f5dd5c5f9e1b0cbec4f646ea65dfc8f2d6
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-02-13 11:32:39 +0000

    Update codemirror documentation

--- packages/codemirror.js/addon/components/codemirror.js
+++ packages/codemirror.js/addon/components/codemirror.js
@@ -51,73 +51,73 @@ export default class extends Component {
 		});
 
 		this.editor.on('change', (instance, change) => {
-			element.dispatchEvent( new Event("change", {
+			element.dispatchEvent( new Event('change', {
 				detail: { instance, change }
 			}));
 		});
 
 		this.editor.on('changes', (instance, changes) => {
-			element.dispatchEvent( new Event("changes", {
+			element.dispatchEvent( new Event('changes', {
 				detail: { instance, changes }
 			}));
 		});
 
 		this.editor.on('beforeChange', (instance, changes) => {
-			element.dispatchEvent( new Event("beforeChange", {
+			element.dispatchEvent( new Event('beforeChange', {
 				detail: { instance, changes }
 			}));
 		});
 
 		this.editor.on('cursorActivity', (instance) => {
-			element.dispatchEvent( new Event("cursorActivity", {
+			element.dispatchEvent( new Event('cursorActivity', {
 				detail: { instance }
 			}));
 		});
 
 		this.editor.on('keyHandled', (instance, name, event) => {
-			element.dispatchEvent( new Event("keyHandled", {
+			element.dispatchEvent( new Event('keyHandled', {
 				detail: { instance, name, event }
 			}));
 		});
 
 		this.editor.on('beforeSelectionChange', (instance, selection) => {
-			element.dispatchEvent( new Event("beforeSelectionChange", {
+			element.dispatchEvent( new Event('beforeSelectionChange', {
 				detail: { instance, selection }
 			}));
 		});
 
 		this.editor.on('viewportChange', (instance, from, to) => {
-			element.dispatchEvent( new Event("viewportChange", {
+			element.dispatchEvent( new Event('viewportChange', {
 				detail: { instance, from, to }
 			}));
 		});
 
 		this.editor.on('gutterClick', (instance, line, gutter, event) => {
-			element.dispatchEvent( new Event("gutterClick", {
+			element.dispatchEvent( new Event('gutterClick', {
 				detail: { instance, line, gutter, event }
 			}));
 		});
 
 		this.editor.on('gutterContextMenu', (instance, line, gutter, event) => {
-			element.dispatchEvent( new Event("gutterContextMenu", {
+			element.dispatchEvent( new Event('gutterContextMenu', {
 				detail: { instance, line, gutter, event }
 			}));
 		});
 
 		this.editor.on('focus', (instance, event) => {
-			element.dispatchEvent( new Event("focus", {
+			element.dispatchEvent( new Event('focus', {
 				detail: { instance, event }
 			}));
 		});
 
 		this.editor.on('blur', (instance, event) => {
-			element.dispatchEvent( new Event("blur", {
+			element.dispatchEvent( new Event('blur', {
 				detail: { instance, event }
 			}));
 		});
 
 		this.editor.on('scroll', (instance) => {
-			element.dispatchEvent( new Event("scroll", {
+			element.dispatchEvent( new Event('scroll', {
 				detail: { instance }
 			}));
 		});
