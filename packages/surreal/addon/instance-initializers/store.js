7d6c3f54364172b8a075637fc93de9abd8c5662e
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-02-16 22:12:22 +0000

    Inject the store into all routes

--- packages/surreal/addon/instance-initializers/store.js
+++ packages/surreal/addon/instance-initializers/store.js
@@ -7,6 +7,9 @@ export default {
 		// Instantiate the store service
 		instance.lookup('service:store');
 
+		// Inject the store into all routes
+		instance.application.inject('route', 'store', 'service:store');
+
 	},
 
 }
