54ed726c675b4ef253bbbb17cb441e6e75cb5494
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-02-21 01:50:47 +0000

    Ensure dropping works, even with children elements

--- packages/dragdrop/addon/components/droppable.js
+++ packages/dragdrop/addon/components/droppable.js
@@ -7,20 +7,13 @@ export default class extends Component {
 	@tracked dropping = false;
 
 	@action didDragOver(event) {
-		event.preventDefault();
-		event.stopPropagation();
-	}
-
-	@action didDragEnter(event) {
 		this.dropping = true;
 		event.preventDefault();
 		event.stopPropagation();
-	}
-
-	@action didDragLeave(event) {
-		this.dropping = false;
-		event.preventDefault();
-		event.stopPropagation();
+		clearTimeout(this.timer);
+		this.timer = setTimeout( () => {
+			this.dropping = false;
+		}, 150);
 	}
 
 	@action didDrop(event) {
