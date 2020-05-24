00e89d43894f79ee792a6cb3bf7060ddd0cac558
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-05-24 17:47:40 +0100

    Add @ascua/metrics logging for amplitude

---
+++ packages/metrics/addon/metrics/amplitude.js
@@ -0,0 +1,102 @@
+import Metric from './base';
+import { assert } from '@ember/debug';
+
+const src = 'script[src*="amplitude"]';
+
+export default class extends Metric {
+
+	name = 'amplitude';
+
+	load() {
+
+		if (window.amplitude) return;
+
+		if (!this.config.id) return;
+
+		/* eslint-disable */
+		(function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
+		;r.type="text/javascript"
+		;r.integrity="sha384-RsEu4WZflrqYcEacpfoGSib3qaSvdYwT4D+DrWqeBuDarSzjwUQR1jO8gDiXZd0E"
+		;r.crossOrigin="anonymous";r.async=true
+		;r.src="https://cdn.amplitude.com/libs/amplitude-6.2.0-min.gz.js"
+		;r.onload=function(){if(!e.amplitude.runQueuedFunctions){
+		console.log("[Amplitude] Error: could not load SDK")}}
+		;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
+		;function s(e,t){e.prototype[t]=function(){
+		this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}
+		var o=function(){this._q=[];return this}
+		;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
+		;for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[]
+		;return this}
+		;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
+		;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
+		;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","enableTracking","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","groupIdentify","onInit","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
+		;function v(e){function t(t){e[t]=function(){
+		e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
+		for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){
+		e=(!e||e.length===0?"$default_instance":e).toLowerCase()
+		;if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]}
+		;e.amplitude=n})(window,document);
+		/* eslint-enable */
+
+		window.amplitude.getInstance().init(this.config.id);
+
+	}
+
+	willDestroy() {
+
+		document.querySelectorAll(src).forEach(e => {
+			e.parentElement.removeChild(e);
+		});
+
+		delete window.amplitude;
+
+	}
+
+	clear() {
+
+		this.load();
+
+		if (!window.amplitude) return;
+
+		window.amplitude.getInstance().clearUserProperties();
+
+	}
+
+	identify(id, data) {
+
+		assert(`You must pass an 'id' as the first argument to ${this.toString()}:identify()`, id);
+
+		this.load();
+
+		if (!window.amplitude) return;
+
+		window.amplitude.getInstance().setUserId(id);
+
+		window.amplitude.getInstance().setUserProperties(data);
+
+	}
+
+	trackPage(data) {
+
+		this.load();
+
+		if (!window.amplitude) return;
+
+		window.amplitude.getInstance().logEvent('page', data);
+
+	}
+
+	trackEvent(name, data) {
+
+		this.load();
+
+		if (!window.amplitude) return;
+
+		assert(`You must pass a 'name' as the first argument to ${this.toString()}:trackEvent()`, name);
+
+		window.amplitude.getInstance().logEvent(name, data);
+
+	}
+
+}
