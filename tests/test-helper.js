b8e33b78353d3818d03465f2a0f19946329454a6
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-08-27 11:52:17 +0100

    Upgrade to ember-cli@3.20.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'ascua/app';
+import config from 'ascua/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
