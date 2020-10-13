596f9edf9c16b1bcb782ea92b5bd7e01d342a5b6
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-10-13 19:01:38 +0100

    Deployed 16d43134

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-!function(){"use strict";self.addEventListener("install",()=>{self.skipWaiting()}),self.addEventListener("activate",()=>{self.clients.matchAll().then(e=>{e.forEach(e=>{e.navigate(e.url)})})})}();
+!function(){"use strict";self.addEventListener("install",(()=>{self.skipWaiting()})),self.addEventListener("activate",(()=>{self.clients.matchAll().then((e=>{e.forEach((e=>{e.navigate(e.url)}))}))}))}();
 //# sourceMappingURL=sw.map
