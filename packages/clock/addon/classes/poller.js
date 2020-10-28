ac02ae07b41fd41d190e097e58dbdaf58e97f934
Tobie Morgan Hitchcock <tobie@abcum.com>
2020-10-28 21:33:20 +0000

    Don’t run poller in FastBoot

--- packages/clock/addon/classes/poller.js
+++ packages/clock/addon/classes/poller.js
@@ -1,14 +1,35 @@
+function enabled() {
+	try {
+		if (typeof FastBoot !== 'undefined') throw "exception";
+		if (!window) throw "exception";
+		if (!window.clearInterval) throw "exception";
+		if (!window.setInterval) throw "exception";
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
 export default class extends Object {
 
 	interval = 1000;
 
 	clear() {
-		clearInterval(this.poller);
+
+		if (enabled() === true) {
+			clearInterval(this.poller);
+		}
+
 	}
 
 	start(ctx, func, ...args) {
+
 		func.apply(ctx, args);
-		this.poller = setInterval(func.bind(ctx, ...args), this.interval);
+
+		if (enabled() === true) {
+			this.poller = setInterval(func.bind(ctx, ...args), this.interval);
+		}
+
 	}
 
 }
