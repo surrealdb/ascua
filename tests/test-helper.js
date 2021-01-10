b7d30e1fa16fc87d57570a5950216f9ecd2c876b
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-01-10 13:30:43 +0000

    Update to ember-cli@3.24.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'ascua/app';
 import config from 'ascua/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
