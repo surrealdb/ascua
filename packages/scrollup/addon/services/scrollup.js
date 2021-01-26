6aefe08304278874a112c47341ec3c5006324774
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-01-26 20:36:17 +0000

    Ensure scroll-to-top works on older browsers
    On older browsers, ScrollToOptions (https://developer.mozilla.org/en-US/docs/Web/API/ScrollToOptions) is not necessarily supported, and therefore we need to use window.scrollTo() with 2 arguments (https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo).

--- packages/scrollup/addon/services/scrollup.js
+++ packages/scrollup/addon/services/scrollup.js
@@ -12,7 +12,11 @@ export default class extends Service {
 		if (typeof FastBoot !== 'undefined') return;
 
 		this.router.on('routeDidChange', () => {
-			window.scrollTo({ top: 0, left: 0, behaviour: 'smooth' });
+			try {
+				window.scrollTo({ top: 0, left: 0, behaviour: 'smooth' });
+			} catch (e) {
+				window.scrollTo(0, 0);
+			}
 		});
 
 	}
