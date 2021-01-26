e87b98e7b0e31988fdb91c2eae219e128c138802
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-01-26 12:54:26 +0000

    Ensure all 3rd party analytics libraries are loaded in defer mode
    This speeds up the initial render, by making sure that 3rd party scripts do not prevent DOM rendering once they have loaded asynchronously.

--- packages/metrics/addon/metrics/intercom.js
+++ packages/metrics/addon/metrics/intercom.js
@@ -16,7 +16,7 @@ export default class extends Metric {
 		if (!this.config.id) return;
 
 		/* eslint-disable */
-		(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;
+		(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.defer=true;
 		s.src=`https://widget.intercom.io/widget/${this.config.id}`;
 		var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};
 		if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
