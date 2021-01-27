06c9bd8ad1a5baccd7613461e1e035cb0688f538
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-01-27 15:15:25 +0000

    Improve @ascua/image package
    The @ascua/image package now works with Ember FastBoot, rendering images on the initial page load. Images now fade-in when the image is fully loaded, and in addition, images will be hidden if the src attribute is modified, and will fade-in once more when the image has finished loading.

--- packages/image/addon/components/image.js
+++ packages/image/addon/components/image.js
@@ -1,13 +1,25 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { arg } from '@ascua/decorators';
 
 export default class extends Component {
 
+	@arg display = (typeof FastBoot !== 'undefined');
+
 	@tracked visible = false;
 
-	@action didEnterViewport() {
+	@action didRerender(el) {
+		el.removeAttribute('display');
+		el.removeAttribute('visible');
+	}
+
+	@action didEnterViewport(el) {
 		this.visible = true;
+	}
+
+	@action didFinishLoading(el) {
+		el.target.setAttribute('visible', '');
 	}
 
 }
