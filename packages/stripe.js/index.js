fe8cca95b108acc1cec82fa4003988b4dbd833d8
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-02-01 07:24:58 +0000

    Preload Stripe.js V3 script in head

--- packages/stripe.js/index.js
+++ packages/stripe.js/index.js
@@ -8,4 +8,10 @@ module.exports = {
 		return tree;
 	},
 
+	contentFor(type) {
+		if (type === 'head') {
+			return '<link rel="preload" as="script" href="https://js.stripe.com/v3/">';
+		}
+	},
+
 };
