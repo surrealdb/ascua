23d6c409e36341ec9f608a3cf9403f980bffda22
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-02-22 21:36:56 +0000

    Add ember-cli import transform for ES5 transpilation

--- packages/transform/index.js
+++ packages/transform/index.js
@@ -1,19 +1,30 @@
 'use strict';
 
 const Stew = require('broccoli-stew');
+const Transpile = require('broccoli-babel-transpiler');
 
 module.exports = {
 
 	name: require('./package').name,
 
 	importTransforms() {
+
+		const babel = this.project.findAddonByName('ember-cli-babel');
+
 		return {
+			babel(tree) {
+				return Transpile(tree, {
+					...babel.buildBabelOptions(),
+					sourceType: 'script',
+				});
+			},
 			fastboot(tree) {
 				return Stew.map(tree, (v) => {
 					return `if (typeof FastBoot === "undefined") { ${v} }`;
 				});
-			}
-		}
+			},
+		};
+
 	},
 
 };
