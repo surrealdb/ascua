9097f5b587067089cf68fa772cd4ccde513de128
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-03-31 11:12:37 +0100

    Update to newer Expect-CT header format

--- packages/server/src/plugins/headers.js
+++ packages/server/src/plugins/headers.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
 	res.removeHeader('x-powered-by');
 
 	res.set('Cache-Control', 'no-store, must-revalidate, max-age=0');
-	res.set('Expect-CT', 'enforce; max-age=86400');
+	res.set('Expect-CT', 'enforce, max-age=86400');
 	res.set('Feature-Policy', "payment 'self';");
 	res.set('Referrer-Policy', 'no-referrer');
 	res.set('Strict-Transport-Security', 'max-age=31536000');
