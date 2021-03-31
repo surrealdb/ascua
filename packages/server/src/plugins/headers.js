e78e52a9b4b9940cf9a632c043ebc90f2e602b90
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-03-31 21:07:25 +0100

    Add default Permissions-Policy header to server

--- packages/server/src/plugins/headers.js
+++ packages/server/src/plugins/headers.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
 	res.set('Cache-Control', 'no-store, must-revalidate, max-age=0');
 	res.set('Expect-CT', 'enforce, max-age=86400');
 	res.set('Feature-Policy', "payment 'self';");
+	res.set('Permissions-Policy', "payment=(self);");
 	res.set('Referrer-Policy', 'no-referrer');
 	res.set('Strict-Transport-Security', 'max-age=31536000');
 	res.set('X-Content-Type-Options', 'nosniff');
