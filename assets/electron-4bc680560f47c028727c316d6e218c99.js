20a6d3f52159a308323286ae07af3f88e7bc8ad5
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-05-08 17:38:57 +0100

    Deployed a1d604f6

---
+++ assets/electron-4bc680560f47c028727c316d6e218c99.js
@@ -0,0 +1,1 @@
+"undefined"!=typeof process&&(e=>{e.ELECTRON=!0,e.node={},e.node.module=e.module,e.node.process=e.process,e.node.require=e.require,delete e.module,delete e.process,delete e.require})(window)
