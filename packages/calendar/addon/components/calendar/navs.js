dc8054f65e0b17e23a908bee3c56649b01f19230
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-07-14 12:56:57 +0100

    Enable quickly moving between years in calendar popup

--- packages/calendar/addon/components/calendar/navs.js
+++ packages/calendar/addon/components/calendar/navs.js
@@ -3,6 +3,14 @@ import { action } from '@ember/object';
 
 export default class extends Component {
 
+	get years() {
+		let b = new Date().getFullYear() - 90;
+		let e = new Date().getFullYear() + 10;
+		return new Array(e - b).fill(0).map( (v, i) => {
+			return String(e - i);
+		});
+	}
+
 	@action prev() {
 		let date = this.args.calendar.pivot;
 		let copy = new Date(date.getTime());
@@ -14,6 +22,13 @@ export default class extends Component {
 		let date = this.args.calendar.pivot;
 		let copy = new Date(date.getTime());
 		copy.setMonth(date.getMonth() + 1);
+		this.args.calendar.current = copy;
+	}
+
+	@action year(year) {
+		let date = this.args.calendar.pivot;
+		let copy = new Date(date.getTime());
+		copy.setYear(year);
 		this.args.calendar.current = copy;
 	}
 
