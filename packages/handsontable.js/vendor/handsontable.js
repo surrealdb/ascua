9f942ddf72ceead096e17d79d2b05694325b9954
Tobie Morgan Hitchcock <tobie@abcum.com>
2021-09-30 10:02:41 +0100

    Fix bug with handsontable.js package in Fastboot

--- packages/handsontable.js/vendor/handsontable.js
+++ packages/handsontable.js/vendor/handsontable.js
@@ -10,7 +10,7 @@
 	}
 
 	generateModule('handsontable', {
-		'default': typeof Handsontable === 'handsontable' ? null : Handsontable,
+		'default': typeof Handsontable === 'undefined' ? null : Handsontable,
 	});
 
 })();
