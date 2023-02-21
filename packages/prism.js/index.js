b950b08ca19350fbfea01e694163154b0320ba05
Emmanuel Keller <ekeller@open-search-server.com>
2023-02-21 15:18:46 +0000

    Update prism to 1.29.0 Add toolbar and copy-to-clipboard plugins

--- packages/prism.js/index.js
+++ packages/prism.js/index.js
@@ -1,33 +1,41 @@
 'use strict';
 
 module.exports = {
-
 	name: require('./package').name,
 
 	included(app) {
-
 		this._super.included.apply(this, ...arguments);
 
 		this.opts = app.options.prism || {};
 
 		app.import(`node_modules/prismjs/prism.js`);
 
+		if (this.opts.copyToClipboard) {
+			app.import('node_modules/prismjs/plugins/toolbar/prism-toolbar.js');
+			app.import(
+				'node_modules/prismjs/plugins/toolbar/prism-toolbar.css'
+			);
+			app.import(
+				'node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js'
+			);
+		}
+
 		if (this.opts.languages) {
-			[].concat(this.opts.languages).forEach( (l) => {
+			[].concat(this.opts.languages).forEach((l) => {
 				app.import(`node_modules/prismjs/components/prism-${l}.js`);
 			});
 		}
 
 		if (this.opts.theme) {
-			app.import(`node_modules/prismjs/themes/prism-${this.opts.theme}.css`);
+			app.import(
+				`node_modules/prismjs/themes/prism-${this.opts.theme}.css`
+			);
 		} else {
 			app.import(`node_modules/prismjs/themes/prism.css`);
 		}
 
 		app.import('vendor/prism.js', {
-			exports: { prism: ['default'] }
+			exports: { prism: ['default'] },
 		});
-
 	},
-
 };
