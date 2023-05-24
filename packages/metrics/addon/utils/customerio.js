195f4fd3ac04688d6179fd371c364f09f35af92f
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-05-24 22:07:36 +0100

    Fix bugs in customerio metrics implementation

--- packages/metrics/addon/utils/customerio.js
+++ packages/metrics/addon/utils/customerio.js
@@ -1,11 +1,11 @@
 export function original(config) {
 
 	/* eslint-disable */
-	var _cio = _cio || [];
+	window._cio = window._cio || [];
 	(function() {
-		var a,b,c;a=function(f){return function(){_cio.push([f].
+		var a,b,c;a=function(f){return function(){window._cio.push([f].
 		concat(Array.prototype.slice.call(arguments,0)))}};b=["load","identify",
-		"sidentify","track","page"];for(c=0;c<b.length;c++){_cio[b[c]]=a(b[c])};
+		"sidentify","track","page"];for(c=0;c<b.length;c++){window._cio[b[c]]=a(b[c])};
 		var t = document.createElement('script'),s = document.getElementsByTagName('script')[0];
 		t.async = true; t.id = 'cio-tracker';
 		t.setAttribute('data-site-id', config.id);
