9918aa400a0c027e009ab186f5b187878b631c18
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-05-24 21:42:50 +0100

    Add support for customer.io tracking

---
+++ packages/metrics/addon/metrics/customerio.js
@@ -0,0 +1,80 @@
+import Metric from './base';
+import { assert } from '@ember/debug';
+import script from '@ascua/metrics/utils/customerio';
+
+const src = 'script[src*="customer"]';
+
+export default class extends Metric {
+
+	name = 'customer';
+
+	init() {
+
+		super.init(...arguments);
+
+		if (window._cio) return;
+
+		if (!this.config.id) return;
+
+		switch (this.config.optimised) {
+		case true:
+			script.optimised(this.config);
+			break;
+		default:
+			script.original(this.config);
+			break;
+		}
+
+	}
+
+	willDestroy() {
+
+		document.querySelectorAll(src).forEach(e => {
+			e.parentElement.removeChild(e);
+		});
+
+		delete window._cio;
+
+	}
+
+	clear() {
+
+		if (!window._cio) return;
+
+		window._cio.reset();
+
+	}
+
+	identify(id, data) {
+
+		if (!window._cio) return;
+
+		assert(`You must pass an 'id' as the first argument to ${this.toString()}:identify()`, id);
+
+		let event = Object.assign({}, data, {
+			user_id: id,
+		});
+
+		window._cio.identify(event);
+
+	}
+
+	trackPage(data) {
+
+		if (!window._cio) return;
+
+		window._cio.page(undefined, data);
+
+	}
+
+	trackEvent(name, data) {
+
+		if (!window._cio) return;
+
+		assert(`You must pass a 'name' as the first argument to ${this.toString()}:trackEvent()`, name);
+
+		window._cio.track(name, data);
+
+	}
+
+}
---
+++ packages/metrics/addon/utils/customerio.js
@@ -0,0 +1,32 @@
+export function original(config) {
+
+	/* eslint-disable */
+	var _cio = _cio || [];
+	(function() {
+		var a,b,c;a=function(f){return function(){_cio.push([f].
+		concat(Array.prototype.slice.call(arguments,0)))}};b=["load","identify",
+		"sidentify","track","page"];for(c=0;c<b.length;c++){_cio[b[c]]=a(b[c])};
+		var t = document.createElement('script'),s = document.getElementsByTagName('script')[0];
+		t.async = true; t.id = 'cio-tracker';
+		t.setAttribute('data-site-id', config.id);
+		t.setAttribute('data-base-url', 'https://eu.customerioforms.com');
+		t.setAttribute('data-use-array-params', 'true');
+		t.setAttribute('data-auto-track-page', 'false');
+		t.setAttribute('data-use-in-app', 'true');
+		t.src = 'https://assets.customer.io/assets/track-eu.js';
+		s.parentNode.insertBefore(t, s);
+	})();
+	/* eslint-enable */
+
+}
+
+export function optimised(config) {
+
+	original(config);
+
+}
+
+export default {
+	original,
+	optimised,
+}
