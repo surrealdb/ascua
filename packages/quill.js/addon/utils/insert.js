50ca941e43eed1ae0189a049ce519da93288a012
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-11-26 17:53:07 +0000

    Temporary changes

--- packages/quill.js/addon/utils/insert.js
+++ packages/quill.js/addon/utils/insert.js
@@ -19,20 +19,48 @@ export default class Insert {
 
 	async insert(file) {
 
-		if (file.type.match(/^image\/(gif|jpg|jpeg|png|apng|svg|webp|bmp)$/i)) {
+		// TODO make this support videos and ensure that files are not embedded unless specifically configured to do so
 
+		if (file.type.match(/^image\/(.*)$/i)) {
 			if (typeof this.handler === 'function') {
 				const src = await this.handler(file);
 				const idx = (this.quill.getSelection() || {}).index;
 				if (idx < 0) idx = this.quill.getLength();
 				this.quill.insertEmbed(idx, 'image', src, 'user');
-			} else {
+			} else if (this.handler === 'embed') {
 				const src = await insert(file);
 				const idx = (this.quill.getSelection() || {}).index;
 				if (idx < 0) idx = this.quill.getLength();
 				this.quill.insertEmbed(idx, 'image', src, 'user');
 			}
+		}
 
+		if (file.type.match(/^audio\/(.*)$/i)) {
+			if (typeof this.handler === 'function') {
+				const src = await this.handler(file);
+				const idx = (this.quill.getSelection() || {}).index;
+				if (idx < 0) idx = this.quill.getLength();
+				this.quill.insertEmbed(idx, 'audio', src, 'user');
+			} else if (this.handler === 'embed') {
+				const src = await insert(file);
+				const idx = (this.quill.getSelection() || {}).index;
+				if (idx < 0) idx = this.quill.getLength();
+				this.quill.insertEmbed(idx, 'audio', src, 'user');
+			}
+		}
+
+		if (file.type.match(/^video\/(.*)$/i)) {
+			if (typeof this.handler === 'function') {
+				const src = await this.handler(file);
+				const idx = (this.quill.getSelection() || {}).index;
+				if (idx < 0) idx = this.quill.getLength();
+				this.quill.insertEmbed(idx, 'video', src, 'user');
+			} else if (this.handler === 'embed') {
+				const src = await insert(file);
+				const idx = (this.quill.getSelection() || {}).index;
+				if (idx < 0) idx = this.quill.getLength();
+				this.quill.insertEmbed(idx, 'video', src, 'user');
+			}
 		}
 
 	}
