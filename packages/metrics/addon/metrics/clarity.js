2fba4591f372c68d9158f50e09a284c039593501
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-02-09 09:23:32 +0000

    Add clarity analytics

---
+++ packages/metrics/addon/metrics/clarity.js
@@ -0,0 +1,35 @@
+import Metric from './base';
+import script from '@ascua/metrics/utils/clarity';
+
+const src = 'script[src*="clarity"]';
+
+export default class extends Metric {
+
+	name = 'clarity';
+
+	init() {
+
+		super.init(...arguments);
+
+		if (!this.config.id) return;
+
+		switch (this.config.optimised) {
+			case true:
+				script.optimised(this.config);
+				break;
+			default:
+				script.original(this.config);
+				break;
+		}
+
+	}
+
+	willDestroy() {
+
+		document.querySelectorAll(src).forEach(e => {
+			e.parentElement.removeChild(e);
+		});
+
+	}
+
+}
---
+++ packages/metrics/addon/utils/clarity.js
@@ -0,0 +1,42 @@
+export function original(config) {
+
+	/* eslint-disable */
+	(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", config.id);
+	/* eslint-enable */
+
+}
+
+export function optimised(config) {
+
+	if (document.readyState === 'complete') {
+
+		let script = document.createElement('script');
+		script.src = `https://www.clarity.ms/tag/${config.id}`;
+		script.async = false;
+		script.defer = true;
+		document.head.appendChild(script);
+
+	} else {
+
+		document.addEventListener('readystatechange', function(event) {
+			if (event.target.readyState === 'complete') {
+				let script = document.createElement('script');
+				script.src = `https://www.clarity.ms/tag/${config.id}`;
+				script.async = false;
+				script.defer = true;
+				document.head.appendChild(script);
+			}
+		});
+
+	}
+
+}
+
+export default {
+	original,
+	optimised,
+}
